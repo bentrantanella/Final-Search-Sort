c8258e1dc9df7cd8eac2db3b58ada21db545f2f4
bentrantanella <bentrantanella@172.18.59.86>
2020-03-05 13:54:02 -0500

    3/5

--- Final Search and Sort Program/src/SortSearchGUI.java
+++ Final Search and Sort Program/src/SortSearchGUI.java
@@ -10,6 +10,18 @@ public class SortSearchGUI extends GBFrame {
 		frm.setVisible(true);
 	}
 	
+	JLabel addstudentLabel = addLabel("Add Student:",1,1,2,1);
+	JLabel addwidgetLabel = addLabel("Add Widget:",1,3,2,1);
+	JLabel addempLabel = addLabel("Add Employee:",1,5,2,1);
 	
+	JLabel snameLabel = addLabel("Name:",2,1,1,1);
+	JTextField snameField = addTextField("",2,2,1,1);
+	JLabel wnumLabel = addLabel("Product number:",2,3,1,1);
+	IntegerField wnumField = addIntegerField(0,2,4,1,1);
+	JLabel enameLabel = addLabel("Name:",2,5,1,1);
+	JTextField enameField = addTextField("",2,6,1,1);
+	
+	JLabel sgpaLabel = addLabel("GPA:",3,1,1,1);
+	DoubleField = addDoubleField(0,3,2,1,1);
 
 }
